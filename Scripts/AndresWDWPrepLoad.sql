0ef99360f86c979a6fe63530622b5d86b1a23fed
Andres Castillo <41123495+andrescastillol@users.noreply.github.com>
2018-07-20 09:45:21 -0600

    Update AndresWDWPrepLoad.sql

--- Scripts/AndresWDWPrepLoad.sql
+++ Scripts/AndresWDWPrepLoad.sql
@@ -1,4 +1,5 @@
 --AndresWDW prep for schema changes (add historical columns for SCD if they do not exist)
+
 IF COL_LENGTH('DimCustomer','HistoryStartDate') IS NULL
  BEGIN
   ALTER TABLE DimCustomer
